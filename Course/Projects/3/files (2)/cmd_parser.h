90e3d9215be74352d4799465b66c9f8a86f543ac
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2026-03-04 13:25:53 -0600

    Auto commit on save

--- Course/Projects/3/files (2)/cmd_parser.h
+++ Course/Projects/3/files (2)/cmd_parser.h
@@ -1,35 +1,26 @@
-/*
- * COMP7500 - Advanced Operating Systems
- * Project 3: AUbatch
- *
- * Maha
- * Auburn University
- *
- * cmd_parser.h
- */
 
 #ifndef CMD_PARSER_H
 #define CMD_PARSER_H
 
-#define CMD_OK     0
+#define CMD_OK 0
 #define CMD_EINVAL 1
-#define CMD_E2BIG  2
+#define CMD_E2BIG 2
 #define CMD_ENOENT 3
 
-#define MAXARGS    10
+#define MAXARGS 10
 #define MAXCMDLINE 256
 
 /* main loop - reads input and dispatches commands */
 void run_command_loop(void);
 
 /* individual command handlers */
-int cmd_help    (int nargs, char **args);
-int cmd_run     (int nargs, char **args);
-int cmd_list    (int nargs, char **args);
-int cmd_fcfs    (int nargs, char **args);
-int cmd_sjf     (int nargs, char **args);
+int cmd_help(int nargs, char **args);
+int cmd_run(int nargs, char **args);
+int cmd_list(int nargs, char **args);
+int cmd_fcfs(int nargs, char **args);
+int cmd_sjf(int nargs, char **args);
 int cmd_priority(int nargs, char **args);
-int cmd_test    (int nargs, char **args);
-int cmd_quit    (int nargs, char **args);
+int cmd_test(int nargs, char **args);
+int cmd_quit(int nargs, char **args);
 
 #endif
